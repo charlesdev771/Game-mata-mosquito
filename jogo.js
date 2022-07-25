1f231dd5b15d717cda8b90b76888e6475d43688a
charlesdev771 <charlesdev0721@protonmail.com>
2022-07-25 16:32:40 -0300

    add dinamic zies

--- jogo.js
+++ jogo.js
@@ -30,6 +30,23 @@ function posicaoRandomica()
     mosquito.style.position = 'absolute';
     
     document.body.appendChild(mosquito);
+
+    console.log(tamanhoAleatorio())
+}
+
+function tamanhoAleatorio()
+{
+    var classe = Math.random() * 3;
+
+    switch(classe)
+    {
+        case 0:
+            return 'mosquito1';
+        case 1:
+            return 'mosquito2';
+        case 2:
+            return 'mosquito3';
+    }
 }
 
 
